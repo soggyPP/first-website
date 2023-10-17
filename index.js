94317f76c2623ec26ab6a371efb09a730303e3f6
simplystudios <new.anshwadhwa@gmail.com>
2023-10-17 19:37:36 +0530

    fixed html code and added responsive css and improved js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('hello world');
+function openlink(){
+    console.log("darke is a loser....{hahhaha}")
+    alert("You Are Entering Ansh Wadhwa's Website as this site is taken over by him")
+    window.open("https://anshwadhwa.vercel.app/links",)
+}
+
+// drake is dumb
